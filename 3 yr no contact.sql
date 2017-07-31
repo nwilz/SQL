b5c01a4de4971f5bbb0b02451b57ee0ae2397adb
nwilz <30605168+nwilz@users.noreply.github.com>
2017-07-31 15:16:31 -0500

    Update 3 yr no contact.sql

--- 3 yr no contact.sql
+++ 3 yr no contact.sql
@@ -1,3 +1,5 @@
+--This query pull records who made a pledge or refused to make a pledge for the last 3 fiscal years
+
 select interface_id_number from unf17.prospect_base
 where id_number in(select id_number from unf17.prospect_contact 
 	where last_result in('Unsp Pldg','Spec Pldg','No Pledge'))
