a3d7c2f525f27c031999f22008cb762228533329
nwilz <30605168+nwilz@users.noreply.github.com>
2017-07-31 16:28:05 -0500

    Update update inactive.sql

--- 155984/update inactive.sql
+++ 155984/update inactive.sql
@@ -1,6 +1,5 @@
-select * from campus.desig_codes
---update campus.desig_codes set active = 0
+update campus.desig_codes set active = 0
 where group_display_name = 'UDEL18' and active = 1
-and (desig_code not in(select interface_id_number from UDEL18.client_update)
-or desig_code in(select interface_id_number from UDEL18.client_update where match2 <> 'R'))
-and desig_code <> 'OTHER' and desig_code <> '00UD0006';
+  and (desig_code not in(select interface_id_number from UDEL18.client_update)
+  or desig_code in(select interface_id_number from UDEL18.client_update where match2 <> 'R'))
+  and desig_code <> 'OTHER' and desig_code <> '00UD0006';
